06742a899f155e086bb74bc54279eaa37f85f5ce
unknown <karol.sujeta@outlook.com>
2019-10-30 13:55:29 +0100

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Lista zadań do zrobienia przed premierą klipu:';
+  todos = [
+    {
+      label: 'Cięcie materiału',
+      priority: 1
+    },
+
+    {
+      label: 'Znalezienie muzyki',
+      priority: 2
+    },
+
+    {
+      label: 'Intro',
+      priority: 3
+    },
+
+    {
+      label: 'Trailer',
+      priority: 4
+    },
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+    }
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo){
+    this.todos=this.todos.filter(t=>t.label!==todo.label);
+  }
 }
